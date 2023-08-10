c20dfe5f73798369cc79cc56f8ce349286395c29
nathantum <tumkipsang@gmail.com>
2023-08-10 03:09:39 -0500

    solved daily sql

---
+++ app_click_thru_rate.sql
@@ -0,0 +1,20 @@
+-- Assume you have an events table on Facebook app analytics. 
+-- Write a query to calculate the click-through rate (CTR) for the app in 2022 
+-- and round the results to 2 decimal places.
+
+-- Definition and note:
+
+-- Percentage of click-through rate (CTR) = 100.0 * Number of clicks / Number of impressions
+-- To avoid integer division, multiply the CTR by 100.0, not 100.
+
+-- Solution --
+SELECT app_id,
+ ROUND(100.0 * 
+  SUM(CASE 
+    WHEN event_type = 'click' THEN 1 ELSE 0 END)
+  / SUM(CASE 
+    WHEN event_type = 'impression' THEN 1 ELSE 0 END), 2) AS ctr_rate
+FROM events
+WHERE timestamp >= '2022-01-01' 
+  AND timestamp < '2023-01-01'
+GROUP BY app_id;
